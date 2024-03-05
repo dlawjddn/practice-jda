6fafc72fd43be2c4df69ee129afa7ef8ba1f3fdf
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-05 23:01:40 +0900

    init project

---
+++ src/test/java/lets/practice/jda/JdaApplicationTests.java
@@ -0,0 +1,13 @@
+package lets.practice.jda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
