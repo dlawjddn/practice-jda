a57adf495b3f36012f72d7657a9a1e3553b865a1
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-08 18:39:07 +0900

    Feat: discord chat listener 추가

--- src/main/java/lets/practice/jda/listener/DiscordListener.java
+++ src/main/java/lets/practice/jda/listener/DiscordListener.java
@@ -1,2 +1,20 @@
-package lets.practice.jda.listener;public class DiscordListener {
+package lets.practice.jda.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+@Slf4j
+public class DiscordListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        User user = event.getAuthor();
+        TextChannel textChannel = event.getChannel().asTextChannel();
+        Message message = event.getMessage();
+        log.info("discord message: {}", message.getContentDisplay());
+    }
 }
