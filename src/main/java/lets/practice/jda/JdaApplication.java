817a64cb81cd8109d8edb0fea4ba06502a1d75cb
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-08 18:38:48 +0900

    Feat: discord 기본 설정 추가

--- src/main/java/lets/practice/jda/JdaApplication.java
+++ src/main/java/lets/practice/jda/JdaApplication.java
@@ -1,13 +1,29 @@
 package lets.practice.jda;
 
+import lets.practice.jda.dto.DiscordBotToken;
+import lets.practice.jda.listener.DiscordListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.MessageActivity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JdaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JdaApplication.class, args);
+        ApplicationContext context = SpringApplication.run(JdaApplication.class, args);
+
+        JDABuilder.createDefault(context.getBean(DiscordBotToken.class).getToken())
+                .setActivity(Activity.playing("START UP VALLEY"))
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new DiscordListener())
+                .build();
+
     }
 
 }
