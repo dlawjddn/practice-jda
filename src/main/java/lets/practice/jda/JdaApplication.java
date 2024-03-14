5c1902c2c2c75146903d0283da4debba795bd651
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-14 16:14:02 +0900

    Feat: 질문 슬래시 커멘드 추가

--- src/main/java/lets/practice/jda/JdaApplication.java
+++ src/main/java/lets/practice/jda/JdaApplication.java
@@ -29,7 +29,11 @@ public class JdaApplication {
         jda.updateCommands().addCommands(
                 Commands.slash("업무시작", "오늘 업무를 시작해요!"),
                 Commands.slash("업무종료", "오늘 업무를 종료해요! 오늘의 업무 내역을 적어주세요!")
-                        .addOption(OptionType.STRING, "work_list", "오늘의 업무 내역을 ', ' 을 통해 구분하여 줄글로 작성해주세요!", true)
+                        .addOption(OptionType.STRING, "work_list", "오늘의 업무 내역을 ', ' 을 통해 구분하여 줄글로 작성해주세요!", true),
+                Commands.slash("질문하기", "업무 상황에 관련하여 질문해요")
+                        .addOption(OptionType.STRING, "question_word", "질문의 내용을 포괄하는 단어를 입력해주세요!", true)
+                        .addOption(OptionType.USER, "to", "질문을 받는 상대방을 지정해주세요", true)
+                        .addOption(OptionType.STRING, "content", "질문의 자세한 내용을 알려주세요!", true)
         ).queue();
 
     }
